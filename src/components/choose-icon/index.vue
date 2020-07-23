0e18f235c54c8e70bbe32e5f77fc3a371e1b1e7a
dropliu <13594448678@163.com>
2020-07-23 23:53:10 +0800

    项目管理:增加修改

---
+++ src/components/choose-icon/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="choose-icon-container">
+    <div class="icon-box" @click="visible=true">
+      <img v-if="curr" :src="curr"/>
+      <d2-icon v-else name='plus'/>
+    </div>
+    <el-dialog title="选择图标" class="icon-dialog"
+      :visible.sync="visible" :modal='false'>
+      <div class="list" >
+        <img class="item" v-for="(item,index) in list" :key="'icon'+index" 
+        @click="choose(item)" :src="item"/>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import api from '@/api'
+export default {
+    name:'choose-icon',
+    props:{
+      init: String,
+      handle: Function
+    },
+    data() {
+      return {
+        list:[],
+        curr: '',
+        visible: false
+      }
+    },
+    mounted(){
+      this.curr = this.init?this.init:''
+      this.getIcon()
+    },
+    methods:{
+      getIcon(){
+        api.PROJECT_ICON().then(res=>{
+          this.list = res
+        })
+      },
+      choose(item){
+        // console.log('click',e)
+        this.curr = item
+        this.handle(item)
+      }
+    }
+}
+</script>
+
+<style lang='scss'>
+.choose-icon-container{
+  .icon-box{
+    width: 100px;
+    height: 100px;
+    text-align: center;
+    line-height: 100px;
+    border: 1px solid #ccc;
+    &:hover{
+      border-color: #999;
+      color: #666;
+    }
+    img{
+      width: 100%;
+    }
+  }
+  .icon-dialog{
+    z-index: 9999!important;
+    .list{
+      display: flex;
+      // justify-content: space-around;
+      .item{
+        width: 48px;
+        height: 48px;
+        margin: 5px 10px;
+      }
+    }
+  }
+}
+</style>
--- src/views/resource/project/pages/product/index.vue
+++ src/views/resource/project/pages/product/index.vue
@@ -39,12 +39,79 @@
         </div>
       </div>
       <div class="list-body">
-        <div class="add-btn">
+        <div class="add-btn" @click="newProduct">
             <span class="btn"><d2-icon name="plus"/>新增</span>
         </div>
-        
+        <div v-if="product.list&&product.list.length>0" class="list">
+        </div>
+        <div v-else class="no-data">
+          <d2-icon name="file-o"/>
+          <div class="text">No data</div>
+        </div>
       </div>
     </div>
+    <el-dialog title="编辑产品" class="product-edit-dialog"
+      :visible.sync="detail.visible" >
+        <el-form :model="detail.data" status-icon :rules="detail.rules" label-width="50px"
+          ref="productEditForm" class="product-edit-form">
+          <el-form-item label="名称" prop="name">
+            <el-input v-model="detail.data.name" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="描述" prop="description">
+            <el-input v-model="detail.data.description" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="标签" prop="tag">
+            <el-select v-model="detail.data.tag"
+              multiple filterable allow-create default-first-option
+              placeholder="请添加产品标签">    
+            </el-select>
+          </el-form-item>
+          <el-form-item label="协议" prop="type">
+            <el-select v-model="detail.data.protocol" no-match-text='请选择' placeholder="请选择">
+              <el-option label="TCP" :value="0"></el-option>
+              <el-option label="MQTT" :value="1"></el-option>
+              <el-option label="HTTP" :value="2"></el-option>
+              <el-option label="COAP" :value="3"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="类型" prop="type">
+            <el-select v-model="detail.data.type" no-match-text='请选择' placeholder="请选择">
+              <el-option label="通用" :value="1"></el-option>
+              <!-- <el-option label="批量项目" :value="2"></el-option> -->
+            </el-select>
+          </el-form-item>
+          <el-form-item label="属性" class="param">
+            <div class="param-box">
+              <div class="item add-btn"><d2-icon name="plus"/>新增</div>
+              <div class="item">
+                <span class="field"></span>
+                <span class="type"></span>
+                <span class="default"></span>
+                <span class="descr"></span>
+                <span class="close"></span>
+              </div>
+            </div>
+          </el-form-item>
+          <el-form-item label="图标">
+            <!-- <div class="icon-add-btn">
+                  <img v-if="detail.data.logo&&detail.data.logo.length>0" :src="detail.data.logo">
+                  <div class="icon-box" @click="icon.visible=true">
+                    <img class="img" v-if="detail.data.logo&&detail.data.logo.length>0" 
+                    :src="detail.data.logo"/>
+                    <d2-icon v-else name='plus'/>
+                  </div>
+              </div> -->
+          </el-form-item>
+        </el-form>
+    </el-dialog>
+    <!-- 选择图标 -->
+    <!-- <el-dialog title="选择图标" class="icon-dialog"
+      :visible.sync="icon.visible" >
+      <div class="list" >
+        <img class="item" v-for="(item,index) in icon.list" :key="'icon'+index" 
+        @click="chooseIcon(item)" :src="item"/>
+      </div>
+    </el-dialog> -->
   </d2-container>
 </template>
 
@@ -66,6 +133,11 @@ export default {
         current: 1,
         size: 10,
         total: 0
+      },
+      detail:{
+        data: {},
+        visible: false,
+        rules:[]
       }
     }
   },
@@ -85,6 +157,18 @@ export default {
         that.product.list = res.records
         that.product.total = res.total
       })
+    },
+    newProduct(){
+      this.detail.data = {
+        // "projectId": 1,
+        "name": "",
+        "type": "",
+        "logo": "",
+        "tag": "",
+        "parameters": [],
+        "description": ""
+      }
+      this.detail.visible = true
     }
   }
 }
@@ -164,6 +248,7 @@ export default {
         line-height: 32px;
         font-size: 14px;
         color: rgba(0, 0, 0, 0.65);
+        cursor: pointer;
         &:hover{
           border: 1px dashed #40475c;
           color: #40475c;
@@ -174,6 +259,34 @@ export default {
           }
         }
       }
+      .no-data{
+        text-align: center;
+        padding: 20px 0;
+        color: #aaa;
+        .text{
+          margin-top:10px
+        }
+      }
+    }
+  }
+  .product-edit-dialog{
+    .product-edit-form{
+     .param{
+       .param-box{
+         .add-btn{
+           border: 1px dashed #ddd;
+           text-align: center;
+            color: #999;
+           &:hover{
+             border-color: #999;
+             color: #555;
+           }
+           .fa{
+             margin-right: 6px;
+           }
+         }
+       }
+     } 
     }
   }
 }
